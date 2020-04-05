c21a8e3ea3058fac84324edc37c3bc436ec7cd37
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-04-04 21:42:40 -0700

    add four basic method heads

---
+++ RecommenderSystem.py
@@ -0,0 +1,11 @@
+def algorithmAforProject(X):
+    return # to be implemented
+
+def algorithmBforProject(X):
+    return # to be implemented
+
+def getSimilarityBetweenTwoUsersNaive:
+    return # to be implemented (parameters will be figured out)
+
+def getSimilarityBetweenTwoUsersEfficient:
+    return # to be implemented (parameters will be figured out)
