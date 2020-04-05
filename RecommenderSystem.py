1360fcdc8bf6f257dbf678c7cbee6ee7a8ad4897
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-04-04 22:01:00 -0700

    add basic test data and define parameters of getSimilarity functions

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -1,11 +1,38 @@
-def algorithmAforProject(X):
-    return # to be implemented
+basicTestData = {
+    "targetUser": "userNameOfTargetUser",
+    "user1": {
+        "likes": ["NJKNO", "HHNONI", "BNJBIO"],
+        "dislikes": ["HJKBK", "NJUINIO"]
+    },
+    "userNameOfTargetUser": {
+        "likes": ["DNLNIL", "NJNJS", "DSQDQSD", "DSQDSQDQS"],
+        "dislikes": ["DQSDQS", "DSQDSQ", "DSQDQS", "DSQDQ"]
+    },
+    "user2": {
+        "likes": ["IIJJKNL", "FDNLNIL"],
+        "dislikes": ["IIJJKNL", "FDNLNIL", "NNIKNI"]
+    }
+}
 
-def algorithmBforProject(X):
-    return # to be implemented
 
-def getSimilarityBetweenTwoUsersNaive():
-    return # to be implemented (parameters will be figured out)
+def algorithmAforProject(inputData):
+    similarityScores = []
+    targetUserLikes = inputData[inputData["targetUser"]]["likes"]
+    targetUserDislikes = inputData[inputData["targetUser"]]["dislikes"]
+    # for i in range(0, len(inputData)):
+    #     int similarityScore = getSimilarityBetweenTwoUsersNaive()
 
-def getSimilarityBetweenTwoUsersEfficient():
-    return # to be implemented (parameters will be figured out)
+
+def algorithmBforProject(inputData):
+    return  # to be implemented
+
+
+def getSimilarityBetweenTwoUsersNaive(targetUserLikes, targetUserDislikes, otherUserLikes, otherUserDisklikes):
+    return  # to be implemented (parameters will be figured out)
+
+
+def getSimilarityBetweenTwoUsersEfficient(targetUserLikes, targetUserDislikes, otherUserLikes, otherUserDisklikes):
+    return  # to be implemented (parameters will be figured out)
+
+
+algorithmAforProject(basicTestData)
